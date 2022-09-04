57b0d06bc80f5cc7a6acc016d5b48b839de6689f
Matheus Viana <matheus_o_viana@hotmail.com>
2022-09-03 22:43:59 -0300

    created AutoMapperSettings

---
+++ src/Movian.Api/Settings/AutoMapperSetting.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using Movian.Api.Models;
+using Movian.Business.Models;
+
+namespace Movian.Api.Settings
+{
+  public class AutoMapperSetting : Profile
+  {
+    public AutoMapperSetting()
+    {
+      CreateMap<Product, ProductDto>().ReverseMap();
+      CreateMap<Supplier, SupplierDto>().ReverseMap();
+      CreateMap<Address, AddressDto>().ReverseMap();
+
+      CreateMap<Product, ProductDto>()
+        .ForMember(p => p.SupplierName,
+                   t => t.MapFrom(s => s.Supplier.Name));
+    }
+  }
+}
