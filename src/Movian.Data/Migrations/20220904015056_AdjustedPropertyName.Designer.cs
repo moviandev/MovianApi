a5d0cd8765345f91a5869bb1a532c13171e3b8ae
Matheus Viana <matheus_o_viana@hotmail.com>
2022-09-03 22:54:04 -0300

    added migrations for adjusting typo in supplierType field

--- src/Movian.Data/Migrations/20220904015056_AdjustedPropertyName.Designer.cs
+++ src/Movian.Data/Migrations/20220904015056_AdjustedPropertyName.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Movian.Business.EnumTypes;
 using Movian.Data.Contexts;
@@ -11,9 +12,10 @@ using Movian.Data.Contexts;
 namespace Movian.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220904015056_AdjustedPropertyName")]
+    partial class AdjustedPropertyName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
---
+++ src/Movian.Data/Migrations/20220904015056_AdjustedPropertyName.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movian.Data.Migrations
+{
+    public partial class AdjustedPropertyName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SuplierType",
+                table: "TB_Supplier",
+                newName: "SupplierType");
+
+            migrationBuilder.RenameColumn(
+                name: "CompleteAddress",
+                table: "TB_Address",
+                newName: "StreetName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SupplierType",
+                table: "TB_Supplier",
+                newName: "SuplierType");
+
+            migrationBuilder.RenameColumn(
+                name: "StreetName",
+                table: "TB_Address",
+                newName: "CompleteAddress");
+        }
+    }
+}
